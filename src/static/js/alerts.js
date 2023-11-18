9b49989303a4b94b5a6d0da960c9221ff179d184
Caleb Ely <le717@users.noreply.github.com>
2023-11-18 14:58:04 -0500

    darken light theme primary link color

--- src/static/js/alerts.js
+++ src/static/js/alerts.js
@@ -1,6 +1,6 @@
 const qAreaAlerts = document.querySelector(".area-alerts");
 
-qAreaAlerts.addEventListener("click", function (e) {
+qAreaAlerts?.addEventListener("click", function (e) {
   // Did we click the close button on an alert?
   if (e.target.matches(".msg-alert .btn-close")) {
     // Delete the clicked alert
--- src/static/js/search.js
+++ src/static/js/search.js
@@ -1,5 +1,5 @@
 // TODO: Change this to a JSON import
-import { default as prompts } from "./prompts.js";
+// import { default as prompts } from "./prompts.js";
 
 
 function findParentElement(element, selector) {
@@ -19,27 +19,23 @@ function findParentElement(element, selector) {
 }
 
 // Create the ability to toggle among search tabs
-const qSearchTabs = document.querySelector(".form-search-tabs");
-qSearchTabs.addEventListener("click", function (e) {
+document.querySelector(".form-search-tabs")?.addEventListener("click", function (e) {
   // Find the clicked tab
   let ele = findParentElement(e.target, ".form-search-tabs .tab:not(.active)");
   if (ele) {
     // Switch the active tab
-    document
-      .querySelector(".form-search-tabs .tab.active")
-      .classList.remove("active");
+    document.querySelector(".form-search-tabs .tab.active")?.classList.remove("active");
     ele.classList.add("active");
 
     // Show the desired search form
     let searchType = ele.classList[1];
-    document.querySelector(".form-search.active").classList.remove("active");
-    document
-      .querySelector(".form-search." + searchType)
-      .classList.add("active");
+    document.querySelector(".form-search.active")?.classList.remove("active");
+    document.querySelector(`.form-search.${searchType}`)?.classList.add("active");
   }
 });
 
 // Create a prettier select element
+// @ts-ignore
 tail.select(document.querySelector("#input-search-host"), {
   search: true,
   // placeholder: "ArthurUnkTweets",
@@ -51,22 +47,22 @@ const qForm = document.querySelector(".page-search form");
 const qSearchQuery = document.querySelector(".search-input");
 const qSearchResults = document.querySelector(".search-results");
 
-qForm.addEventListener("submit", function(e) {
-  e.preventDefault();
+// qForm?.addEventListener("submit", function(e) {
+//   e.preventDefault();
 
-  // No blank or one character input
-  let input = qSearchQuery.value.trim();
-  if (!input || input.length === 1) {
-    return false;
-  }
+//   // No blank or one character input
+//   let input = qSearchQuery?.value.trim();
+//   if (!input || input.length === 1) {
+//     return false;
+//   }
 
-  // Search for the input in the film data
-  let query = new RegExp(input, "i");
-  prompts.forEach(film => {
-    if (query.test(film.title)) {
-      let filmYear = film.date.split("-")[0];
-      let html = `<a href="films/${filmYear}/${film.id}.html">${film.title}</a>`;
-      qSearchResults.insertAdjacentHTML("beforebegin", html);
-    }
-  });
-});
+//   // Search for the input in the film data
+//   let query = new RegExp(input, "i");
+//   prompts.forEach(prompt => {
+//     if (query.test(prompt.title)) {
+//       let filmYear = prompt.date.split("-")[0];
+//       let html = `<a href="films/${filmYear}/${prompt.id}.html">${prompt.title}</a>`;
+//       qSearchResults?.insertAdjacentHTML("beforebegin", html);
+//     }
+//   });
+// });
